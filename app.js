b0b95bbfdbcb7585e5813658b400fe0fb2971f06
tlaudahl <travis.laudahl@gmail.com>
2020-04-22 19:31:32 -0500

    Added static public folder, moved css to external files

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
