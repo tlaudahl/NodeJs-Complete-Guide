43cd76ed8ed046cf824a54b73565296fc888c979
tlaudahl <travis.laudahl@gmail.com>
2020-04-29 14:41:53 -0500

    Added a product model with Sequelize, updated some routes to use this model instead of writing to files

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const path = require('path');
+const path = require("path");
 
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const pageNotFound = require('./controllers/404');
-const db = require('./util/database');
+const pageNotFound = require("./controllers/404");
+const sequelize = require("./util/database");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
+app.use(pageNotFound.pageNotFound);
 
-
-app.use(pageNotFound.pageNotFound)
-
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,81 +1,99 @@
-const Product = require('../models/product');
-const Cart = require('../models/cart');
+const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res) => {
-    Product.fetchAll()
-    .then(([rows]) => {
-        res.render('shop/product-list', {
-            pageTitle: 'All Products',
-            path: '/products',
-            prods: rows
-        })
+  Product.findAll()
+    .then((products) => {
+      res.render("shop/product-list", {
+        pageTitle: "All Products",
+        path: "/products",
+        prods: products,
+      });
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
 
 exports.getProduct = (req, res, next) => {
-    const { productId } = req.params;
-    Product.findById(productId).then(([product]) => {
-        res.render('shop/product-detail.ejs', { product: product[0], pageTitle: product.title, path: "/products" });
-    }).catch(err => console.log(err));
-}
+  const { productId } = req.params;
+  //   Product.findAll({ where: { id: productId } }) // Same as below
+  //     .then((products) => {
+  //       res.render("shop/product-detail.ejs", {
+  //         product: products[0],
+  //         pageTitle: products[0].title,
+  //         path: "/products",
+  //       });
+  //     })
+  //     .catch((err) => console.log(err));
+  Product.findByPk(productId)
+    .then((product) => {
+      res.render("shop/product-detail.ejs", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll()
-    .then(([rows, fieldData]) => {
-        res.render('shop/index', {
-            pageTitle: 'Shop',
-            path: '/',
-            prods: rows
-        })
+  Product.findAll()
+    .then((products) => {
+      res.render("shop/index", {
+        pageTitle: "Shop",
+        path: "/",
+        prods: products,
+      });
     })
-    .catch(err => {console.log(err)});
-}
+    .catch((err) => console.log(err));
+};
 
 exports.getCart = (req, res, next) => {
-    Cart.getCart(cart => {
-        Product.fetchAll(products => {
-            const cartProducts = [];
-            for (product of products) {
-                const cartProductData = cart.products.find(prod => prod.id === product.id);
-            if (cartProductData) {
-                cartProducts.push({ productData: product, qty: cartProductData.qty });
-            }}
-            res.render('shop/cart', {
-                path: '/cart',
-                pageTitle: 'Your Cart',
-                products: cartProducts
-            });
-        });
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cartProducts,
+      });
     });
+  });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
-    const { productId } = req.body;
-    Product.findById(productId, product => {
-        Cart.deleteProduct(productId, product.price);
-        res.redirect('/cart');
-    });
+  const { productId } = req.body;
+  Product.findById(productId, (product) => {
+    Cart.deleteProduct(productId, product.price);
+    res.redirect("/cart");
+  });
 };
 
 exports.postCart = (req, res, next) => {
-    const { productId } = req.body;
-    Product.findById(productId, product => {
-        Cart.addProduct(productId, product.price);
-    })
-    res.redirect('/cart');
+  const { productId } = req.body;
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, product.price);
+  });
+  res.redirect("/cart");
 };
 
 exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout'
-    })
-}
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: "Your Orders"
-    })
-}
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
