5b83efeb3fb2a808dfeda6c96b85c2cdd77df25c
tlaudahl <travis.laudahl@gmail.com>
2020-05-01 16:27:39 -0500

    Updated everything but cart to use mongoose

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const pageNotFound = require("./controllers/404");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
@@ -20,9 +21,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("5eab8ad9d1fcca986330df47")
+  User.findById("5eac90be5d4d2996f0aea4df")
     .then((user) => {
-      req.user = new User(user.username, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch((err) => console.log(err));
@@ -33,6 +34,23 @@ app.use(shopRoutes);
 
 app.use(pageNotFound.pageNotFound);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-0ab5b.mongodb.net/shop?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Travis",
+          email: "travis@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,8 +2,9 @@ const Product = require("../models/product");
 const User = require("../models/user");
 
 exports.getProducts = (req, res, next) => {
-  Product.getAllProducts()
+  Product.find()
     .then((products) => {
+      console.log(products);
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
@@ -29,7 +30,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.getAllProducts()
+  Product.find()
     .then((products) => {
       res.render("shop/index", {
         prods: products,
