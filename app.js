ad918cc01bbc8d21eb1106fbc9f55c2e8fd8be8a
tlaudahl <travis.laudahl@gmail.com>
2020-05-12 11:17:10 -0500

    Added very simple login page and route. Updated CSS to match section 14

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(pageNotFound.pageNotFound);
 
